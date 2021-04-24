37681dc80200ae58a8990a765e2cf364438b2fb6
Sven <sven.bergendahl@outlook.de>
2021-04-24 10:38:26 +0200

    added challenge02

--- challenge02/main.tf
+++ challenge02/main.tf
@@ -1,5 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.40.0"
+    }
+  }
+   backend "azurerm" {
+    resource_group_name  = "Terraform-Course"
+    storage_account_name = "cloudshell123155"
+    container_name       = "terraformstate"
+    key                  = "dev.terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
-  features {}
+  features {  }
 }
 
 resource "azurerm_resource_group" "myresourcegroup"{ 
